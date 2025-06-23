060828dd192303e1e8d097be1a1cfa409f41e0a0
dhreetirayyy <dkumar.deo@gmail.com>
2025-06-22 19:03:16 -0700

    Weather

---
+++ Vari-func.py
@@ -0,0 +1,6 @@
+def weather():
+    print("The weather isn't quite that bad in", nashvillie)
+    print("Its really hot in", fairytown)
+nashvillie = "fairytown"
+fairytown = "nashvillie"
+weather()
