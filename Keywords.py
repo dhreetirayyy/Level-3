7db984933de38417c26c0a317d72922cfcdd0f36
dhreetirayyy <dkumar.deo@gmail.com>
2025-07-20 18:50:03 -0700

    Keywords - Break,Pass,Continue

---
+++ Keywords.py
@@ -0,0 +1,14 @@
+for i in range(19):
+    if i==13:
+        break
+    print(i)
+
+for j in range(20):
+    if j==16:
+        pass
+    print(j)
+
+for k in range(15):
+    if k==8:
+        continue
+    print(k)
