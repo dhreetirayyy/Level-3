40acf22e6f9883db0b0128454f8e58defa023d76
dhreetirayyy <dkumar.deo@gmail.com>
2025-11-30 17:57:31 -0800

    Intersection

---
+++ setsintersection.py
@@ -0,0 +1,8 @@
+set_a = {"break", "shop", "fun"}
+set_b = {"break", "sleep", "friends"}
+print("Orignal sets:")
+print(set_a)
+print(set_b)
+print("After sets:")
+set_c = set_a.intersection(set_b)
+print(set_c)
