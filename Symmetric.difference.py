e045471f87a142cecce69e00eeeea2dc27286375
dhreetirayyy <dkumar.deo@gmail.com>
2025-12-07 14:26:36 -0800

    Symmetric Difference

---
+++ Symmetric.difference.py
@@ -0,0 +1,10 @@
+set1 = {'blue','green','pink'}
+set2 = {'purple', 'yellow','blue'}
+print(set1,set2)
+s12 = set1.symmetric_difference(set2)
+print(s12)
+set3 = {11,23,8,4}
+set4 = {9,23,45,10}
+print(set1,set2)
+s34 = set3.symmetric_difference(set4)
+print(s34)
